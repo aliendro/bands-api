a0449d0cb5910b5d51abfc86503b31c447033754
Michael Aliendro <michael.aliendro@dentsu.com>
2023-05-09 18:54:00 -0300

    vercel setup

---
+++ api/index.js
@@ -0,0 +1,54 @@
+const dotenv = require('dotenv');
+const express = require("express");
+const albums = require("../json/albums.json");
+const bands = require("../json/bands.json");
+const tracks = require("../json/tracks.json");
+
+dotenv.config();
+const app = express();
+
+const albumsMap = new Map(albums);
+const bandsMap = new Map(bands);
+const tracksMap = new Map(tracks);
+
+app.get("/api/albums", (_, res) => {
+  res.setHeader('Content-Type', 'application/json');
+  res.setHeader('Cache-Control', 's-max-age=9999, stale-while-revalidate');
+  return res.json([...albumsMap.values()]);
+});
+
+app.get("/api/albums/:id", (req, res) => {
+  res.setHeader('Content-Type', 'application/json');
+  res.setHeader('Cache-Control', 's-max-age=9999, stale-while-revalidate');
+  return res.json(albumsMap.get(req.params.id));
+});
+
+app.get("/api/bands", (_, res) => {
+  res.setHeader('Content-Type', 'application/json');
+  res.setHeader('Cache-Control', 's-max-age=9999, stale-while-revalidate');
+  return res.json([...bandsMap.values()]);
+});
+
+app.get("/api/bands/:id", (req, res) => {
+  res.setHeader('Content-Type', 'application/json');
+  res.setHeader('Cache-Control', 's-max-age=9999, stale-while-revalidate');
+  return res.json(bandsMap.get(req.params.id));
+});
+
+app.get("/api/tracks", (_, res) => {
+  res.setHeader('Content-Type', 'application/json');
+  res.setHeader('Cache-Control', 's-max-age=9999, stale-while-revalidate');
+  return res.json([...tracksMap.values()]);
+});
+
+app.get("/api/tracks/:id", (req, res) => {
+  res.setHeader('Content-Type', 'application/json');
+  res.setHeader('Cache-Control', 's-max-age=9999, stale-while-revalidate');
+  return res.json(tracksMap.get(req.params.id));
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`[debug]: API is running on port ${process.env.PORT}...`);
+})
+
+module.exports = app;
